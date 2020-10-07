06436148d5ce27b66d13becabd70fbc723082607
srkreddydendi <srkreddydendi@gmail.com>
2020-10-07 21:01:51 +0530

    1 file modified

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import { render } from "react-dom";
 //import Savings from "./Savings";
 //import "./styles/style.scss";
 import A from "./shared/A";
+import B from "./shared/B";
 
 class App extends Component {
   render() {
     let items = [{ title: "hello", name:"shivaram" }, { title: "hello",name:"arun" }];
     let C = null;
-    switch (this.props.id) {
+    switch (2) {
       case 1:
         C = A;
         break;
@@ -18,6 +19,8 @@ class App extends Component {
       default:
         C = A; 
     }
+let x = `<h3>hello ${C}</h3>`;
+let y =React.createElement('h1', {}, x);
 
     return (
       <div>
@@ -25,6 +28,8 @@ class App extends Component {
     
         return (
           <>
+          {y}
+          <div dangerouslySetInnerHTML={{__html: x}}></div>
         <span key={index} ><C title={item.title} name={item.name} /></span>
         <span key={index} ><C {...item} /></span>
         </>
